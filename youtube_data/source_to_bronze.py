605749f27ffa8ced4941b3a6fd9e89aace45bb1a
Mateus Cruz <68655740+cruzmateus07@users.noreply.github.com>
2025-02-12 20:01:30 -0300

    added video logic

--- youtube_data/source_to_bronze.py
+++ youtube_data/source_to_bronze.py
@@ -5,31 +5,31 @@ import youtube_auth
 
 
 def get_channel_data(youtube,channel_id,output_path,ct_ltz):
-    """"
-        Function to get Youtube Channel data based on provided channel_id.
-        Saves data to json file on output path of choice.
     """
-    request = youtube.channels().list(
+        Function to get all data associated to Youtube Channel.
+        Channel, Playlists, Playlists Items, Videos
+    """
+
+
+    ## --------------------- Channel Data  --------------------- ##
+    channel_request = youtube.channels().list(
         ## Available channel list parts at: https://developers.google.com/youtube/v3/docs/channels/list
         part = "snippet,contentDetails,statistics",
         id = channel_id
     ).execute()
 
     ## Checks if directory exists, otherwise creates it
-    json_file_path = f"{output_path}channel/"
-    if not os.path.exists(json_file_path):
-        os.makedirs(json_file_path)
+    channel_json_file_path = f"{output_path}channel_{channel_id}/channel_data/"
+    if not os.path.exists(channel_json_file_path):
+        os.makedirs(channel_json_file_path)
     
-    with open(f"{json_file_path}channel_data_{ct_ltz}", "w") as output:
-        json.dump(request, output)
+    ## Saves Channel Data in Json Format Files
+    with open(f"{channel_json_file_path}{ct_ltz}", "w") as output:
+        json.dump(channel_request, output)
 
 
-def get_playlist_data(youtube,channel_id,output_path,ct_ltz):
-    """"
-        Function to get Youtube Playlist data based on provided channel_id.
-        Saves data to json file on output path of choice.
-    """
-    request = youtube.playlists().list(
+    ## --------------------- Playlist Data  --------------------- ##
+    playlist_request = youtube.playlists().list(
         ## Available playlist list parts at: https://developers.google.com/youtube/v3/docs/playlists/list
         part = "snippet,contentDetails",
         channelId = channel_id,
@@ -37,20 +37,71 @@ def get_playlist_data(youtube,channel_id,output_path,ct_ltz):
     ).execute()
 
     ## Checks if directory exists, otherwise creates it
-    json_file_path = f"{output_path}playlist/"
-    if not os.path.exists(json_file_path):
-        os.makedirs(json_file_path)
+    playlist_json_file_path = f"{output_path}channel_{channel_id}/playlist_data/"
+    if not os.path.exists(playlist_json_file_path):
+        os.makedirs(playlist_json_file_path)
 
-    with open(f"{json_file_path}playlist_data_{ct_ltz}", "w") as output:
-        json.dump(request, output)
+    with open(f"{playlist_json_file_path}{ct_ltz}", "w") as output:
+        json.dump(playlist_request, output)
+
+
+    ## --------------------- Playlist Items Data  --------------------- ##
+    ## Checks if directory exists, otherwise creates it
+    playlist_items_json_file_path = f"{output_path}channel_{channel_id}/playlist_items_data/"
+    if not os.path.exists(playlist_items_json_file_path):
+        os.makedirs(playlist_items_json_file_path)
+
+    items = playlist_request.get("items")
+
+    playlist_ids = []
+
+    for item in items:
+        playlist_id = item["id"]
+
+        playlist_item_request = youtube.playlistItems().list(
+            ## Available playlist list parts at: https://developers.google.com/youtube/v3/docs/playlistItems/list
+            part = "snippet,contentDetails",
+            playlistId = playlist_id,
+            maxResults=25
+        ).execute()
+
+        with open(f"{playlist_items_json_file_path}playlist_{playlist_id}_items_{ct_ltz}", "w") as output:
+            json.dump(playlist_item_request, output)
+
+
+        ## --------------------- Video Data  --------------------- ##
+        ## Checks if directory exists, otherwise creates it
+        video_json_file_path = f"{output_path}channel_{channel_id}/video_data/"
+        if not os.path.exists(video_json_file_path):
+            os.makedirs(video_json_file_path)
+
+        items = playlist_item_request.get("items")
+
+        video_ids = []
+
+        for item in items:
+            contents_details = item.get("contentDetails")
+            video_id = contents_details.get("videoId")
+            video_ids += [video_id]
+
+        for id in video_ids:
+            video_request = youtube.videos().list(
+                ## Available playlist list parts at: https://developers.google.com/youtube/v3/docs/playlistItems/list
+                part = "snippet,contentDetails",
+                id = id,
+                maxResults=25
+            ).execute()
+
+            with open(f"{video_json_file_path}video_{id}_{ct_ltz}", "w") as output:
+                json.dump(video_request, output)
 
 
 if __name__ == "__main__":
     ## Input channel_id of choise
-    channel_id = <<youtube_channel_id>>
+    channel_id = 'UC5JY5JhsD4_GA2W6N8_Uaiw'
 
     ## Output path for json file
-    output_path = <<output_file_path>>
+    output_path = '/Users/mateuscruz/Documents/Work/Projects/Warehouse/youtube/bronze/'
 
     ## Auth function call
     youtube = youtube_auth.youtube_auth()
@@ -59,4 +110,3 @@ if __name__ == "__main__":
     ct_ltz = datetime.datetime.now()
 
     get_channel_data(youtube,channel_id,output_path,ct_ltz)
-    get_playlist_data(youtube,channel_id,output_path,ct_ltz)
